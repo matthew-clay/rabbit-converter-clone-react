643d9b059316c74d558a6d11dc0183a4634d96f5
Matthew-Clay <matthewclay.dev@gmail.com>
2022-03-21 22:16:35 +0630

    added toastAlert box when user click copy button

--- src/components/ControlBar.js
+++ src/components/ControlBar.js
@@ -5,28 +5,42 @@ function ControlBar(props) {
     console.log(
       `${valueRef.current.id.toUpperCase()} text copied to clipboard...`
     );
+
+    const toastAlertTag = document.querySelector("#toast-alert");
+    toastAlertTag.style.display = "flex";
+    setTimeout(() => {
+      toastAlertTag.style.display = "none";
+    }, 2000);
   };
 
   return (
-    <div className="flex flex-col items-center lg:flex-row justify-center mr-4">
-      <input
-        type="button"
-        className="bg-amber-400 text-gray-800 active:bg-amber-500 text-md font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none my-3 mr-3"
-        value="Clear"
-        onClick={props.clearAllText}
-      ></input>
-      <input
-        type="button"
-        className="bg-white text-gray-800 active:bg-gray-300 text-md font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none my-3 mr-3"
-        value="Copy Unicode"
-        onClick={() => copyToClipboard(props.unicode)}
-      ></input>
-      <input
-        type="button"
-        className="bg-white text-gray-800 active:bg-gray-300 text-md font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none my-3 mr-3"
-        value="Copy Zawgyi"
-        onClick={() => copyToClipboard(props.zawgyi)}
-      ></input>
+    <div>
+      <div className="flex flex-col items-center lg:flex-row justify-center mr-4">
+        <input
+          type="button"
+          className="bg-amber-400 text-gray-800 active:bg-amber-500 text-md font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none my-3 mr-3"
+          value="Clear"
+          onClick={props.clearAllText}
+        ></input>
+        <input
+          type="button"
+          className="bg-white text-gray-800 active:bg-gray-300 text-md font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none my-3 mr-3"
+          value="Copy Unicode"
+          onClick={() => copyToClipboard(props.unicode)}
+        ></input>
+        <input
+          type="button"
+          className="bg-white text-gray-800 active:bg-gray-300 text-md font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none my-3 mr-3"
+          value="Copy Zawgyi"
+          onClick={() => copyToClipboard(props.zawgyi)}
+        ></input>
+      </div>
+
+      <div className="relative hidden justify-center" id="toast-alert">
+        <span className="absolute z-10 bg-blue-400 px-5 py-4 text-white font-semibold select-none rounded-md">
+          Text copied to clipboard...
+        </span>
+      </div>
     </div>
   );
 }
